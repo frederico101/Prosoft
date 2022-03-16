bdf6c11114f4300290cfcb3c6a7809a86284eb31
frederico101 <ter.frederico.alves@sada.com.br>
2022-03-16 00:43:50 -0300

    Obter Cliente Por id Implementado

--- Prosoft.deveRegister.api/Controllers/UsuarioController.cs
+++ Prosoft.deveRegister.api/Controllers/UsuarioController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Prosoft.devRegister.Business.Interfaces;
 using Prosoft.devRegister.Business.Model;
@@ -11,17 +10,50 @@ namespace Prosoft.devRegister.api.Controllers
     {
         private readonly IUsuarioService _usuarioservice;
         private readonly IUsuarioRepository _usuarioRepository;
-        public UsuariosController(IUsuarioService usuarioservice, IUsuarioRepository usuarioRepository)
+        private readonly ILogger<UsuariosController> _logger;
+        public UsuariosController(IUsuarioService usuarioservice, IUsuarioRepository usuarioRepository, 
+                                  ILogger<UsuariosController> logger)
         {
             _usuarioservice = usuarioservice;
             _usuarioRepository = usuarioRepository;
+            _logger = logger;
         }
 
         [HttpGet("listarUsuarios")]
-        public async Task<IEnumerable<Usuario>> ListarUsuarios()
+        public async Task<ActionResult<IEnumerable<Usuario>>> ListarUsuarios()
         {
-            return await _usuarioRepository.ObterTodos();
-
+            try
+            {
+                var result = await _usuarioRepository.ObterTodos();
+                
+                if (result == null) return BadRequest(new { success = false, data = "" });
+                
+                return Ok(new { success = true, data = result});
+            }
+            catch (Exception ex) {
+                _logger.LogError($"Erro ao listar os usuarios => {ex}");
+            }
+            return NotFound(new { success = false, data = ""});
         }
+
+        [HttpGet("obterUsuarioPorId")]
+        public async Task<ActionResult<Usuario>> ObterUsuarioPorId(string usuarioId)
+        {
+            try
+            {
+                var user = _usuarioservice.ObterUsuarioPorId(usuarioId);
+                var result = await _usuarioRepository.ObterUsuarioPorIdRepository(usuarioId);
+
+                if (result == null) return BadRequest(new { success = false, data = "" });
+
+                return Ok(new { success = true, data = result });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro ao listar os usuarios pelo id => {ex}");
+            }
+            return NotFound(new { success = false, data = "" });
+        }
+
     }
 }
